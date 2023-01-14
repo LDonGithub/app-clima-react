3306c6dd84c92b785cfde6679e8d2ad1a510ad6c
LDonGithub <ldonoso@gmail.cl>
2023-01-14 03:28:01 -0300

    Agrega Buscador base

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './assets/css/App.css';
 import NavBar from './components/NavBar';
+import SearchWeather from './components/SearchWeather';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <SearchWeather/>
     </div>
   );
 }
--- src/components/SearchWeather.js
+++ src/components/SearchWeather.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const SearchWeather = () => {
     return(
-        <form></form>
+        <form>
+            <input />
+            <button >Search</button>
+        </form>
     )
 }
 
